f912a0a66d80cdc6970ee022be0813c17a7b1400
khanh-nh <67193946+khanh-nh@users.noreply.github.com>
2024-12-19 10:40:22 +0700

    Create user_func.c
    This code contains all the user defined functions

---
+++ user_func.c
@@ -0,0 +1,16 @@
+// This code contains all the user defined functions
+
+double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+    const double R = 6371e3; // Earth's radius in meters
+    double phi1 = lat1 * M_PI / 180.0;
+    double phi2 = lat2 * M_PI / 180.0;
+    double deltaPhi = (lat2 - lat1) * M_PI / 180.0;
+    double deltaLambda = (lon2 - lon1) * M_PI / 180.0;
+
+    double a = sin(deltaPhi / 2) * sin(deltaPhi / 2) +
+               cos(phi1) * cos(phi2) *
+               sin(deltaLambda / 2) * sin(deltaLambda / 2);
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+
+    return R * c; // Distance in meters
+}
